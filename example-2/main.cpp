70c6be661654a415a9a64498bec607ed63d9ab81
Chris Young <chris@unsatisfactorysoftware.co.uk>
2018-08-14 12:44:16 +0100

    silly hardcoded locale

--- example-2/main.cpp
+++ example-2/main.cpp
@@ -265,13 +265,13 @@ bool	err;
 	timeSynced.	store (false);
 	timesyncSet.	store (false);
 	run.		store (false);
-	std::wcout.imbue(std::locale("en_US.utf8"));
+	std::wcout.imbue(std::locale("en_GB.utf8"));
 	if (argc == 1) {
 	   printOptions ();
 	   exit (1);
 	}
 
-	std::setlocale (LC_ALL, "en-US.utf8");
+	std::setlocale (LC_ALL, "en-GB.utf8");
 
 //	For file input we do not need options like Q, G and C,
 //	We do need an option to specify the filename
@@ -507,7 +507,7 @@ bool	err;
 	                                         programName << "\n";
 	   run. store (false);
 	}
-*/
+
 
 	run.load();
 /*
