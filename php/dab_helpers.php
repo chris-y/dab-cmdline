0d593fde803541b7aa8b8e27ee2f47bfd487c691
Chris Young <chris@unsatisfactorysoftware.co.uk>
2018-08-28 19:53:07 +0100

    data service types to text

--- php/dab_helpers.php
+++ php/dab_helpers.php
@@ -39,12 +39,46 @@ function dab_prog_type($type) {
   return $progtype[$type];
 }
 
-function dab_is_plus($type) {
-  if($type == 63) {
+function dab_is_plus($ascty) {
+  if($ascty == 63) {
     return true;
   } else {
     return false;
   }
+}
+
+function dab_data_prog_type($ptype) {
+	$type = "unknown";
+
+	if($ptype == 0x002) {
+		$type = "SlideShow";
+	} else if ($ptype == 0x004) {
+		$type = "TPEG";
+	} else if ($ptype == 0x007) {
+		$type = "SPI";
+	} else if ($ptype == 0x009) {
+		$type = "DMB";
+	} else if ($ptype == 0x00d) {
+		$type = "Filecasting";
+	} else if ($ptype == 0x44a) {
+		$type = "Journaline(r)";
+	}
+	
+	return $type;
+}
+
+function dab_data_serv_type($dscty) {
+	$type = "data";
+
+	if($dscty == 5) {
+		$type = "TDC";
+	} else if ($dscty == 24) {
+		$type = "MPEG2";
+	} else if ($dscty == 60) {
+		$type = "MOT";
+	}
+	
+	return $type;
 }
 
 ?>
